186175a0c15b1fa56de76bae6efff8bd8de31ccd
codeofhassan <theultimatehassan@gmail.com>
2024-11-02 16:03:29 +0500

    Did some more work (1)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
 
   const addTodo = () => {
     if (inputValue.trim()) {
@@ -30,9 +32,19 @@ function App() {
     }
   };
 
+  const indexOfLastTodo = currentPage * itemsPerPage;
+  const indexOfFirstTodo = indexOfLastTodo - itemsPerPage;
+  const currentTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo);
+
+  const totalPages = Math.ceil(todos.length / itemsPerPage);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="App">
-      <h1>Todo List</h1>
+      <h1 className='main-heading'>Todo List</h1>
       <input
         type="text"
         value={inputValue}
@@ -41,21 +53,30 @@ function App() {
       />
       <button onClick={addTodo}>Add Todo</button>
       <ul>
-        {todos.map((todo, index) => (
-          <li key={index} className={todo.completed ? 'completed' : ''}>
-            <strong>{index + 1}.</strong>
+        {currentTodos.map((todo, index) => (
+          <li key={index + indexOfFirstTodo} className={todo.completed ? 'completed' : ''}>
+            <strong>{index + indexOfFirstTodo + 1}.</strong>
             <span className="todo-text">{todo.text}</span>
             <div>
-              <button onClick={() => toggleTodo(index)} class="check-buttons">
+              <button onClick={() => toggleTodo(index + indexOfFirstTodo)} className="check-buttons">
                 {todo.completed ? <i className="fas fa-undo"></i> : <i className="fas fa-check"></i>}
               </button>
-              <button onClick={() => deleteTodo(index)}>
+              <button onClick={() => deleteTodo(index + indexOfFirstTodo)}>
                 <i className="fas fa-trash"></i>
               </button>
             </div>
           </li>
         ))}
       </ul>
+      {todos.length >= itemsPerPage && (
+        <div className="pagination">
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button key={index + 1} onClick={() => handlePageChange(index + 1)} className={`page-button ${currentPage === index + 1 ? 'active' : ''}`}>
+              {index + 1}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
